6a47ecd94c1b8f070736a6d58dcff82bd4a5b7e5
Emilio Rodriguez <erdguezh@gmail.com>
2021-12-09 10:59:30 -0500

    Estilos globales del app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.scss'
+// import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello Next.js</h1>
+      <h1>Hello Next.js</h1>
     </>
   )
 }
