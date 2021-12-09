d6ca064346b90a8ed317b65a1c689866866bb0e1
Emilio Rodriguez <erdguezh@gmail.com>
2021-12-09 17:50:17 -0500

    Consumiendo API de Stripe (SSR)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
+
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,12 +28,33 @@ export default function Home() {
           </h1>
           <p>
             Get access to all pulications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1K4trnLXGQjX7M7Skq2MP91Z', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceID: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
